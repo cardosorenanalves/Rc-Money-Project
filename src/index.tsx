cd5a552067023afed882c72561a843235dcd345d
cardosorenanalves <renancardoso0307@gmail.com>
2022-04-19 14:59:31 -0300

    continuação

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,30 @@ createServer({
   models:{
     transaction: Model,
   },
+  
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id: 1,
+          title: "freelance de website",
+          type:"deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2022-04-19 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type:"withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2022-04-05 04:00:00")
+        }
+      ],
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
